db5e0ceebd7faa3afc4c4169bfb726bd5bac27d5
Anirudh Sharma <anirudh03sharma@gmail.com>
2019-01-02 00:09:40 +0530

    Fixing the test case for Counting Sort (Issue # 1) - Included multiple instances of same number to use the counting property

--- src/test/java/org/redquark/algorithms/sorting/CountingSortTest.java
+++ src/test/java/org/redquark/algorithms/sorting/CountingSortTest.java
@@ -11,10 +11,10 @@ public class CountingSortTest {
     public void countingSortIntegerTest() {
 
         // Unsorted integer array
-        Integer[] unsorted = new Integer[]{5, 1, 7, 0, 2, 9, 6, 3, 4, 8};
+        Integer[] unsorted = new Integer[]{1, 4, 1, 2, 7, 5, 2};
 
         // Sorted integer array
-        Integer[] sorted = new Integer[]{0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+        Integer[] sorted = new Integer[]{1, 1, 2, 2, 4, 5, 7};
 
         // Comparing the two integer arrays
         Assert.assertArrayEquals(sorted, countingSort.sort(unsorted));
